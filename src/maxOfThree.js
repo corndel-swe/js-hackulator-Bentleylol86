d9080b9e2248ce6ee5b1238e726d8ce77fb474a3
Bentleylol86 <Laurence.Woodhouse@bentley.co.uk>
2024-11-27 14:03:59 +0000

    maxOfThree.js

--- src/maxOfThree.js
+++ src/maxOfThree.js
@@ -1,15 +1,19 @@
 function maxOfThree(x, y, z) {
-  if (x > y && x > z) {
+  if (x >= y && x >= z) {
     return x
   }
 
-  if (y > z && y > x) {
+  if (y >= z && y >= x) {
     return y
   }
 
-  if (z > x && z > y) {
+  if (z >= x && z >= y) {
     return z
   }
 }
 
 export default maxOfThree
+
+//the By using >= instead of >, we ensure that it correctly identifies 
+//the maximum even when some numbers are equal. 
+//This way, it should handle all potential cases.
