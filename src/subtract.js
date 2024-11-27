bb92d73d9577a388242d1e2442ba7bd327a3f49a
Bentleylol86 <Laurence.Woodhouse@bentley.co.uk>
2024-11-27 12:25:04 +0000

    fixes subtract.js

--- src/subtract.js
+++ src/subtract.js
@@ -5,13 +5,16 @@
  * subtract(5, 5) => 0
  * subtract(-2, 4) => 6
  */
-function subtract(x, y) {
+function subtract (x, y) {
   if (x < y) {
     return y - x
   }
 
   if (y < x) {
     return x - y
+  }
+  if (x == y) {
+  return 0;
   }
 }
 
